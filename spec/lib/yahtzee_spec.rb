5f3351ef3fd973fc13bd3de0554be8349460feeb
Dan Rosenthal <dan@heysparkbox.com>
2015-02-09 13:55:57 -0500

    refactor: comment out scoring tests from categorization tests

--- spec/lib/yahtzee_spec.rb
+++ spec/lib/yahtzee_spec.rb
@@ -6,130 +6,130 @@ describe Category do
   it "is aces" do 
     roll = Category.new(1, 1, 1, 1, 1)
     expect(roll.aces?).to be true
-    expect(roll.score).to be == 5
+    #expect(roll.score).to be == 5
     
     roll = Category.new(1, 1, 1, 4, 4)
     expect(roll.aces?).to be true
-    expect(roll.score).to be == 3
+    #expect(roll.score).to be == 3
     
     roll = Category.new(2, 2, 2, 4, 4) 
     expect(roll.aces?).to be false
-    expect(roll.score).to be == 0
+    #expect(roll.score).to be == 0
   end
   
   it "is twos" do 
     roll = Category.new(2, 2, 2, 1, 1)
     expect(roll.twos?).to be true
-    expect(roll.score).to be == 6
+    #expect(roll.score).to be == 6
     
     roll = Category.new(2, 2, 1, 4, 4)
     expect(roll.twos?).to be true
-    expect(roll.score).to be == 4
+    #expect(roll.score).to be == 4
     
     roll = Category.new(1, 3, 1, 4, 4) 
     expect(roll.twos?).to be false
-    expect(roll.score).to be == 0
+    #expect(roll.score).to be == 0
   end
   
   it "is threes" do 
     roll = Category.new(3, 3, 3, 1, 1)
     expect(roll.threes?).to be true
-    expect(roll.score).to be == 9
+    #expect(roll.score).to be == 9
     
     roll = Category.new(3, 3, 1, 4, 4)
     expect(roll.threes?).to be true
-    expect(roll.score).to be == 6
+    #expect(roll.score).to be == 6
     
     roll = Category.new(1, 2, 1, 4, 4) 
     expect(roll.threes?).to be false
-    expect(roll.score).to be == 0
+    #expect(roll.score).to be == 0
   end
   
   it "is fours" do 
     roll = Category.new(4, 4, 4, 1, 1)
     expect(roll.fours?).to be true
-    expect(roll.score).to be == 12
+    #expect(roll.score).to be == 12
     
     roll = Category.new(4, 4, 1, 2, 2)
     expect(roll.fours?).to be true
-    expect(roll.score).to be == 8
+    #expect(roll.score).to be == 8
     
     roll = Category.new(1, 2, 1, 3, 3) 
     expect(roll.fours?).to be false
-    expect(roll.score).to be == 0
+    #expect(roll.score).to be == 0
   end
   
   it "is fives" do 
     roll = Category.new(5, 5, 5, 1, 1)
     expect(roll.fives?).to be true
-    expect(roll.score).to be == 15
+    #expect(roll.score).to be == 15
     
     roll = Category.new(5, 5, 1, 2, 2)
     expect(roll.fives?).to be true
-    expect(roll.score).to be == 10
+    #expect(roll.score).to be == 10
     
     roll = Category.new(1, 2, 1, 3, 3) 
     expect(roll.fives?).to be false
-    expect(roll.score).to be == 0
+    #expect(roll.score).to be == 0
   end
   
   it "is sixes" do 
     roll = Category.new(6, 6, 6, 1, 1)
     expect(roll.sixes?).to be true
-    expect(roll.score).to be == 18
+    #expect(roll.score).to be == 18
     
     roll = Category.new(6, 6, 1, 2, 2)
     expect(roll.sixes?).to be true
-    expect(roll.score).to be == 12
+    #expect(roll.score).to be == 12
     
     roll = Category.new(1, 2, 1, 3, 3) 
     expect(roll.sixes?).to be false
-    expect(roll.score).to be == 0
+    #expect(roll.score).to be == 0
   end
   
   it "is three of a kind" do 
     roll = Category.new(6, 6, 6, 1, 1)
     expect(roll.three_of_a_kind?).to be true
-    expect(roll.score).to be == 20
+    #expect(roll.score).to be == 20
     
     roll = Category.new(3, 3, 3, 1, 1)
     expect(roll.three_of_a_kind?).to be true
-    expect(roll.score).to be == 11
+    #expect(roll.score).to be == 11
     
     roll = Category.new(1, 2, 3, 4, 5)
     expect(roll.three_of_a_kind?).to be false
-    expect(roll.score).to be == 0
+    #expect(roll.score).to be == 0
   end
   
   it "is four of a kind" do 
     roll = Category.new(6, 6, 6, 6, 1)
     expect(roll.four_of_a_kind?).to be true
-    expect(roll.score).to be == 25
+    #expect(roll.score).to be == 25
     
     roll = Category.new(3, 3, 3, 3, 1)
     expect(roll.four_of_a_kind?).to be true
-    expect(roll.score).to be == 13
+    #expect(roll.score).to be == 13
     
     roll = Category.new(1, 2, 3, 4, 5)
     expect(roll.four_of_a_kind?).to be false
-    expect(roll.score).to be == 0
+    #expect(roll.score).to be == 0
   end
   
   it "is a full house" do 
     roll = Category.new(6, 6, 6, 4, 4)
     expect(roll.full_house?).to be true
-    expect(roll.score).to be == 25
+    #expect(roll.score).to be == 25
     
     roll = Category.new(3, 3, 2, 2, 2)
     expect(roll.full_house?).to be true
-    expect(roll.score).to be == 25
+    #expect(roll.score).to be == 25
     
     roll = Category.new(1, 1, 1, 1, 1)
     expect(roll.full_house?).to be true
-    expect(roll.score).to be == 25
+    #expect(roll.score).to be == 25
     
     roll = Category.new(1, 2, 3, 4, 5)
     expect(roll.full_house?).to be false
-    expect(roll.score).to be == 0
+    #expect(roll.score).to be == 0
   end
 end

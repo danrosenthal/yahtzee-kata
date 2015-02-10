7073bd941e07cc1a646e573ee92b831c490a3785
Dan Rosenthal <dan@heysparkbox.com>
2015-02-10 12:20:39 -0500

    refactor: dry up my methods a bit

--- lib/yahtzee.rb
+++ lib/yahtzee.rb
@@ -5,7 +5,7 @@ class Category
     @roll = args.sort
   end
 
-  # Upper section categories
+  # category methods
 
   def aces?; roll.include?(1); end
   def twos?; roll.include?(2); end
@@ -13,32 +13,14 @@ class Category
   def fours?; roll.include?(4); end
   def fives?; roll.include?(5); end
   def sixes?; roll.include?(6); end
-
-  # Lower section categories
-
   def three_of_a_kind?; call_the_roll(3); end
   def four_of_a_kind?; call_the_roll(4); end
-
-  def full_house?
-    roll.uniq.length <= 2 && (roll[0] == roll[1]) && (roll[-1] == roll[-2])
-  end
-
-  def small_straight?
-    roll[0..3] == [1, 2, 3, 4] || roll[0..3] == [2, 3, 4, 5] || roll[1..4] == [3, 4, 5, 6] || roll[0..3] == [3, 4, 5, 6]
-  end
-
-  def large_straight?
-    roll == [1, 2, 3, 4, 5] || roll == [2, 3, 4, 5, 6]
-  end
-
-  def yahtzee?
-    roll.min == roll.max
-  end
+  def full_house?; roll.uniq.length <= 2 && (roll[0] == roll[1]) && (roll[-2] == roll[-1]); end
+  def small_straight?; roll[0..3] == [1, 2, 3, 4] || roll[0..3] == [2, 3, 4, 5] || roll[1..4] == [3, 4, 5, 6] || roll[0..3] == [3, 4, 5, 6]; end
+  def large_straight?; roll == [1, 2, 3, 4, 5] || roll == [2, 3, 4, 5, 6]; end
+  def yahtzee?; roll.min == roll.max; end
   
-  def to_s
-  end
-  
-  # Upper section scoring methods
+  # scoring methods
   
   def score_aces!; aces? ? count_em_up(1) : 0; end
   def score_twos!; twos? ? count_em_up(2) : 0; end
@@ -46,9 +28,6 @@ class Category
   def score_fours!; fours? ? count_em_up(4) : 0; end
   def score_fives!; fives? ? count_em_up(5) : 0; end
   def score_sixes!; sixes? ? count_em_up(6) : 0; end
-  
-  # Lower section scoring methods
-  
   def score_three_of_a_kind!; three_of_a_kind? ? add_em_up : 0; end
   def score_four_of_a_kind!; four_of_a_kind? ? add_em_up : 0; end
   def score_full_house!; full_house? ? 25 : 0; end
@@ -58,26 +37,12 @@ class Category
   
   private
   
-  def add_em_up; roll.reduce(:+); end
-  
+  def add_em_up; roll.reduce(:+); end 
   def count_em_up(num); roll.count(num) * num; end
-  
+
   def call_the_roll(amt)
-    if roll.count(1) >= amt
-      true
-    elsif roll.count(2) >= amt
-      true
-    elsif roll.count(3) >= amt
-      true
-    elsif roll.count(4) >= amt
-      true
-    elsif roll.count(5) >= amt
-      true
-    elsif roll.count(6) >= amt
-      true
-    else
-      false
-    end
+    (1..6).any? { |dots| roll.count(dots) >= amt }
   end
   
 end
+

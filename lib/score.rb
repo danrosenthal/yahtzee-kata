b1fbc6c118e0ef0482e22f9d2d6664cdd5a32bce
Dan Rosenthal <dan@heysparkbox.com>
2015-02-13 15:01:25 -0500

    test: pass the :aces test and fail the :twos test

---
+++ lib/score.rb
@@ -0,0 +1,45 @@
+class Score
+  
+  attr_accessor :roll_score
+  
+  def initialize(*args)
+    roll = args.sort
+    @roll_score = {
+      :aces => 0,
+      :twos => 0,
+      :threes => 0,
+      :fours => 0,
+      :fives => 0,
+      :sixes => 0,
+      :three_kind => 0,
+      :four_kind => 0,
+      :full_house => 0,
+      :sm_straight => 0,
+      :lg_straight => 0,
+      :yahtzee => 0,
+      :chance => 0
+    }
+    aces(roll)
+    #twos(roll)
+    #threes(roll)
+    #fours(roll)
+    #fives(roll)
+    #sixes(roll)
+    #three_kind(roll)
+    #four_kind(roll)
+    #full_house(roll)
+    #sm_straight(roll)
+    #lg_straight(roll)
+    #yahtzee(roll)
+    #chance(roll)
+  end
+  
+  def aces(roll)
+    if roll.include?(1)
+      roll_score[:aces] = (roll.count(1) * 1)
+    else
+      roll_score[:aces] = 0
+    end
+  end
+
+end
--- spec/lib/yahtzee_spec.rb
+++ spec/lib/yahtzee_spec.rb
@@ -1,6 +1,17 @@
 require "spec_helper"
-require "yahtzee"
+require "score"
 
-describe Category do
-
+describe Score do
+  context "when a roll has 3 aces and 2 twos" do
+    roll = Score.new(1,1,1,2,2)
+    
+    it "adds up the aces for :aces" do
+      expect(roll.roll_score[:aces]).to eq(3)
+    end
+    
+    it "adds up the twos for :twos" do
+      expect(roll.roll_score[:twos]).to eq(4)
+    end
+    
+  end
 end

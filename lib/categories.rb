2a5e037f9fc358484fb40810f19aea92e8699409
Dan Rosenthal <dan@heysparkbox.com>
2015-02-24 15:21:07 -0500

    refactor: make the categories class cleaner

--- lib/categories.rb
+++ lib/categories.rb
@@ -3,28 +3,14 @@ require 'twos'
 require 'threes'
 require 'fours'
 require 'fives'
+require 'sixes'
 
 class Categories
-  
-  attr_reader :roll_score
+  attr_reader :category_score
   
   def initialize(roll)
-    roll
-    @roll_score = {
-      aces: 0,
-      twos: 0,
-      threes: 0,
-      fours: 0,
-      fives: 0,
-      sixes: 0,
-      three_kind: 0,
-      four_kind: 0,
-      full_house: 0,
-      sm_straight: 0,
-      lg_straight: 0,
-      yahtzee: 0,
-      chance: 0
-    }
+    @category_score = {}
+    
     aces(roll)
     twos(roll)
     threes(roll)
@@ -41,57 +27,54 @@ class Categories
   end
   
   def aces(roll)
-    roll_score[:aces] = Aces.new.score(roll)
+    category_score[:aces] = Aces.new.score(roll)
   end
-  
   def twos(roll)
-    roll_score[:twos] = Twos.new.score(roll)
+    category_score[:twos] = Twos.new.score(roll)
   end
-  
   def threes(roll)
-    roll_score[:threes] = Threes.new.score(roll)
+    category_score[:threes] = Threes.new.score(roll)
   end
-  
   def fours(roll)
-    roll_score[:fours] = Fours.new.score(roll)
+    category_score[:fours] = Fours.new.score(roll)
   end
-  
   def fives(roll)
-    roll_score[:fives] = Fives.new.score(roll)
+    category_score[:fives] = Fives.new.score(roll)
+  end
+  def sixes(roll)
+    category_score[:sixes] = Sixes.new.score(roll)
   end
   
-  def sixes(roll)
-    roll_score[:sixes] = (roll.count(6) * 6) if roll.include?(6)
-  end
+  
   
   def three_kind(roll)
-    roll_score[:three_kind] = roll.reduce(&:+) if (1..6).any? { |pips| roll.count(pips) >= 3 }
+    category_score[:three_kind] = roll.reduce(&:+) if (1..6).any? { |pips| roll.count(pips) >= 3 }
   end
   
   def four_kind(roll)
-    roll_score[:four_kind] = roll.reduce(&:+) if (1..6).any? { |pips| roll.count(pips) >= 4 }
+    category_score[:four_kind] = roll.reduce(&:+) if (1..6).any? { |pips| roll.count(pips) >= 4 }
   end
   
   def full_house(roll)
     roll_sorted = roll.sort
-    roll_score[:full_house] = 25 if roll_sorted.uniq.length <= 2 && roll_sorted[0] == roll_sorted[1] && roll_sorted[-2] == roll_sorted[-1]
+    category_score[:full_house] = 25 if roll_sorted.uniq.length <= 2 && roll_sorted[0] == roll_sorted[1] && roll_sorted[-2] == roll_sorted[-1]
   end
   
   def sm_straight(roll)
     roll_sorted = roll.sort
-    roll_score[:sm_straight] = 30 if roll_sorted[0..3] == [1, 2, 3, 4] || roll_sorted[0..3] == [2, 3, 4, 5] || roll_sorted[1..4] == [3, 4, 5, 6] || roll_sorted[0..3] == [3, 4, 5, 6]
+    category_score[:sm_straight] = 30 if roll_sorted[0..3] == [1, 2, 3, 4] || roll_sorted[0..3] == [2, 3, 4, 5] || roll_sorted[1..4] == [3, 4, 5, 6] || roll_sorted[0..3] == [3, 4, 5, 6]
   end
   
   def lg_straight(roll)
     roll_sorted = roll.sort
-    roll_score[:lg_straight] = 40 if roll_sorted == [1, 2, 3, 4, 5] || roll_sorted == [2, 3, 4, 5, 6]
+    category_score[:lg_straight] = 40 if roll_sorted == [1, 2, 3, 4, 5] || roll_sorted == [2, 3, 4, 5, 6]
   end
   
   def yahtzee(roll)
-    roll_score[:yahtzee] = 50 if roll.uniq.length == 1
+    category_score[:yahtzee] = 50 if roll.uniq.length == 1
   end
   
   def chance(roll)
-    roll_score[:chance] = roll.reduce(&:+)
+    category_score[:chance] = roll.reduce(&:+)
   end
 end
--- spec/categories_spec.rb
+++ spec/categories_spec.rb
@@ -6,23 +6,23 @@ describe Categories do
     roll = Categories.new([1,1,1,2,2])
     
     it "adds up the aces for :aces" do
-      expect(roll.roll_score[:aces]).to eq(3)
+      expect(roll.category_score[:aces]).to eq(3)
     end
     
     it "adds up the twos for :twos" do
-      expect(roll.roll_score[:twos]).to eq(4)
+      expect(roll.category_score[:twos]).to eq(4)
     end
     
     it "adds up the dice for :three_kind" do
-      expect(roll.roll_score[:three_kind]).to eq(7)
+      expect(roll.category_score[:three_kind]).to eq(7)
     end
     
     it "scores 25 for :full_house" do
-      expect(roll.roll_score[:full_house]).to eq(25)
+      expect(roll.category_score[:full_house]).to eq(25)
     end
     
     it "scores 7 for :chance" do
-      expect(roll.roll_score[:chance]).to eq(7)
+      expect(roll.category_score[:chance]).to eq(7)
     end
   end
   
@@ -30,23 +30,23 @@ describe Categories do
     roll = Categories.new([3,3,3,4,4])
     
     it "adds up all the threes for :threes" do
-      expect(roll.roll_score[:threes]).to eq(9)
+      expect(roll.category_score[:threes]).to eq(9)
     end
     
     it "adds up all the fours for :fours" do
-      expect(roll.roll_score[:fours]).to eq(8)
+      expect(roll.category_score[:fours]).to eq(8)
     end
     
     it "adds up the threes for :three_kind" do
-      expect(roll.roll_score[:three_kind]).to eq(17)
+      expect(roll.category_score[:three_kind]).to eq(17)
     end
     
     it "scores 25 for :full_house" do
-      expect(roll.roll_score[:full_house]).to eq(25)
+      expect(roll.category_score[:full_house]).to eq(25)
     end
     
     it "scores 17 for :chance" do
-      expect(roll.roll_score[:chance]).to eq(17)
+      expect(roll.category_score[:chance]).to eq(17)
     end
   end
   
@@ -54,23 +54,23 @@ describe Categories do
     roll = Categories.new([5,5,5,6,6])
     
     it "adds up all the fives for :fives" do
-      expect(roll.roll_score[:fives]).to eq(15)
+      expect(roll.category_score[:fives]).to eq(15)
     end
     
     it "adds up all the sixes for :sixes" do
-      expect(roll.roll_score[:sixes]).to eq(12)
+      expect(roll.category_score[:sixes]).to eq(12)
     end
     
     it "adds up the dice for :three_kind" do
-      expect(roll.roll_score[:three_kind]).to eq(27)
+      expect(roll.category_score[:three_kind]).to eq(27)
     end
     
     it "scores 25 for :full_house" do
-      expect(roll.roll_score[:full_house]).to eq(25)
+      expect(roll.category_score[:full_house]).to eq(25)
     end
     
     it "scores 27 for :chance" do
-      expect(roll.roll_score[:chance]).to eq(27)
+      expect(roll.category_score[:chance]).to eq(27)
     end
   end
   
@@ -78,57 +78,57 @@ describe Categories do
     roll = Categories.new([5,5,5,5,6])
     
     it "adds up all the dice for :four-kind" do
-      expect(roll.roll_score[:four_kind]).to eq(26)
+      expect(roll.category_score[:four_kind]).to eq(26)
     end
     
     it "adds up all the fives for :fives" do
-      expect(roll.roll_score[:fives]).to eq(20)
+      expect(roll.category_score[:fives]).to eq(20)
     end
     
     it "adds up all the sixes for :sixes" do
-      expect(roll.roll_score[:sixes]).to eq(6)
+      expect(roll.category_score[:sixes]).to eq(6)
     end
     
     it "scores 26 for :chance" do
-      expect(roll.roll_score[:chance]).to eq(26)
+      expect(roll.category_score[:chance]).to eq(26)
     end
   end
   
   context "when a roll is a straight" do
     roll = Categories.new([1,2,3,4,5])
     it "scores 30 for :sm_straight" do
-      expect(roll.roll_score[:sm_straight]).to eq(30)
+      expect(roll.category_score[:sm_straight]).to eq(30)
     end
     
     it "scores 40 for :lg_straight" do
-      expect(roll.roll_score[:lg_straight]).to eq(40)
+      expect(roll.category_score[:lg_straight]).to eq(40)
     end
     
     it "scores 15 for :chance" do
-      expect(roll.roll_score[:chance]).to eq(15)
+      expect(roll.category_score[:chance]).to eq(15)
     end
   end
   
   context "when a roll is all the same dice" do
     roll = Categories.new([6,6,6,6,6])
     it "scores 50 for :yahtzee" do
-      expect(roll.roll_score[:yahtzee]).to eq(50)
+      expect(roll.category_score[:yahtzee]).to eq(50)
     end
     
     it "scores 30 for :sixes" do
-      expect(roll.roll_score[:sixes]).to eq(30)
+      expect(roll.category_score[:sixes]).to eq(30)
     end
     
     it "scores 30 for :three_kind" do
-      expect(roll.roll_score[:three_kind]).to eq(30)
+      expect(roll.category_score[:three_kind]).to eq(30)
     end
     
     it "scores 30 for :four_kind" do
-      expect(roll.roll_score[:four_kind]).to eq(30)
+      expect(roll.category_score[:four_kind]).to eq(30)
     end
     
     it "scores 30 for :chance" do
-      expect(roll.roll_score[:chance]).to eq(30)
+      expect(roll.category_score[:chance]).to eq(30)
     end
   end
   
